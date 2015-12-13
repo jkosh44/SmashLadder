4a19ccb9a3316f1cb3c55f001debc47a15810a90
jwangsadinata <jwangsadinat@wesleyan.edu>
2015-12-13 14:14:11 +0100

    modified xml files

--- app/src/main/java/hu/ait/android/smashladder/fragment/FragmentPlayerDetail.java
+++ app/src/main/java/hu/ait/android/smashladder/fragment/FragmentPlayerDetail.java
@@ -1,14 +1,18 @@
 package hu.ait.android.smashladder.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import hu.ait.android.smashladder.PlayerDetailActivity;
 import hu.ait.android.smashladder.R;
 import hu.ait.android.smashladder.adapter.PlayerAdapter;
 
@@ -19,8 +23,6 @@ public class FragmentPlayerDetail extends Fragment {
 
     private String playerName;
     private int playerRank;
-
-    private TextView tvDetailPlayer;
 
     public FragmentPlayerDetail() {
 
@@ -36,8 +38,20 @@ public class FragmentPlayerDetail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_player_detail, container, false);
 
-        tvDetailPlayer = (TextView) rootView.findViewById(R.id.tvPlayerDetails);
-        tvDetailPlayer.setText("Tag: " + playerName + "\nRank: " + playerRank);
+        TextView tvDetailPlayer = (TextView) rootView.findViewById(R.id.tvPlayerDetails);
+        tvDetailPlayer.setText(playerName);
+
+        TextView tvDetailRank = (TextView) rootView.findViewById(R.id.tvPlayerRank);
+        tvDetailRank.setText("Rank: " + playerRank);
+
+        Button btnChallenge = (Button) rootView.findViewById(R.id.btnChallenge);
+        btnChallenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Toast.makeText(context, playerName + " has been notified", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
